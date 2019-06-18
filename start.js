11577734cb413b6ff27983be73e869c8e7ae8d91
Jens Bockstal <bockstal.j@hotmail.be>
2019-06-18 20:57:14 +0200

    test gitignore .env

--- start.js
+++ start.js
@@ -42,6 +42,7 @@ var server = app.listen(app.get('port'), function () {
 //-------------------------------------------------------------------
 var FORGE_CLIENT_ID = process.env.FORGE_CLIENT_ID;
 var FORGE_CLIENT_SECRET = process.env.FORGE_CLIENT_SECRET;
+console.log("FORGE_CLIENT_ID: " + process.env.FORGE_CLIENT_ID);
 var access_token = '';
 var scopes = 'data:read data:write data:create bucket:create bucket:read';
 const querystring = require('querystring');
